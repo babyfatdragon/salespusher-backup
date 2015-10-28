9251be43561afc619f0b02ed76fa82e218f906e1
LiZhenshuo <pleasenice@gmail.com>
2015-10-29 03:12:55 +0800

    a

--- src/main/java/com/worksap/salespusher/repository/ProductDocumentRepository.java
+++ src/main/java/com/worksap/salespusher/repository/ProductDocumentRepository.java
@@ -1,11 +1,9 @@
 package com.worksap.salespusher.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.worksap.salespusher.entity.ProductDocumentEntity;
 
 public interface ProductDocumentRepository extends JpaRepository<ProductDocumentEntity,Long>{
-	List<ProductDocumentEntity> findById(int id);
+	ProductDocumentEntity findById(long id);
 }
--- src/main/java/com/worksap/salespusher/repository/ProductImageRepository.java
+++ src/main/java/com/worksap/salespusher/repository/ProductImageRepository.java
@@ -1,11 +1,9 @@
 package com.worksap.salespusher.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.worksap.salespusher.entity.ProductImageEntity;
 
 public interface ProductImageRepository extends JpaRepository<ProductImageEntity,Long> {
-	List<ProductImageEntity> findById(int id);
+	ProductImageEntity findById(long id);
 }
