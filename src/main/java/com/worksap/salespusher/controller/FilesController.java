45413d3e612bd81f139ce8a84986f2d53d9fe935
LiZhenshuo <pleasenice@gmail.com>
2015-10-29 02:59:26 +0800

    asd

--- src/main/java/com/worksap/salespusher/controller/FilesController.java
+++ src/main/java/com/worksap/salespusher/controller/FilesController.java
@@ -12,13 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.worksap.salespusher.entity.ProductDocumentEntity;
 import com.worksap.salespusher.entity.ProductImageEntity;
 import com.worksap.salespusher.repository.ProductDocumentRepository;
 import com.worksap.salespusher.repository.ProductImageRepository;
 
-@Controller
+@RestController
 public class FilesController {
 	private final ProductDocumentRepository productDocumentRepository;
 	private final ProductImageRepository productImageRepository;
@@ -31,7 +33,7 @@ public class FilesController {
 	
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/files/images/upload", method = RequestMethod.POST)
-    public void uploadImages(@RequestParam("file") MultipartFile file,@RequestParam("productId") String productId) throws IOException {
+    public ProductImageEntity uploadImages(@RequestParam("file") MultipartFile file,@RequestParam("productId") String productId) throws IOException {
         if (!file.isEmpty()) {
             try {
             	//String fileDirectory = "C:/Users/li_zh/workspace/sp-files/images/";
@@ -46,28 +48,33 @@ public class FilesController {
 	            stream.write(bytes);
 	            stream.close();
 	            long pId = Long.getLong(productId);
-	            ProductImageEntity p = new ProductImageEntity(fileName,pId);
-	            //this.productImageRepository.save(p);
-	            
+	            ProductImageEntity product = new ProductImageEntity(fileName,pId);
+	            return this.productImageRepository.save(product);
             } catch (Exception e) {
             }
         }
+		return null;
     }
     
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/files/docs/upload", method = RequestMethod.POST)
-    public void uploadDocs(@RequestParam("file") MultipartFile file) throws IOException {
+    public ProductDocumentEntity uploadDocs(@RequestParam("file") MultipartFile file,@RequestParam("productId") String productId) throws IOException {
         if (!file.isEmpty()) {
             try {
+            	//String fileDirectory = "C:/Users/li_zh/workspace/sp-files/images/";
+            	String fileDirectory = "/Users/fatdragon/Repos/sp-files/images/";
                 byte[] bytes = file.getBytes();
-                String fileName = file.getOriginalFilename();
+                String fileName = "product-"+productId+"-"+file.getOriginalFilename();
                 BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File("C:/Users/li_zh/workspace/sp-files/docs/"+fileName)));
+                        new BufferedOutputStream(new FileOutputStream(new File(fileDirectory+fileName)));
                 stream.write(bytes);
                 stream.close();
+	            long pId = Long.getLong(productId);
+	            ProductDocumentEntity product = new ProductDocumentEntity(fileName,pId);
+	            return this.productDocumentRepository.save(product);
             } catch (Exception e) {
             }
         }
-        System.out.println(String.format("receive %s", file.getOriginalFilename()));
+        return null;
     }
 }
